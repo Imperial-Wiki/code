03d50b4c5fd9a739ff537529091f8af73524abef
Divefr <62665359+Divefr@users.noreply.github.com>
2022-09-20 11:59:12 +0100

    Update questionscode.js

--- forum/questionscode.js
+++ forum/questionscode.js
@@ -42,7 +42,7 @@ var retrieveaws = async (key)=>{
 
 async function digestmessage(message) {
     const msgUint8 = new TextEncoder().encode(message);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);
+    const hashBuffer = await crypto.subtle.digest("SHA-384", msgUint8);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, "0")).join("");
     return await hashHex;
